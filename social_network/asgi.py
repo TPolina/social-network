d91cfdf43343142934f54e50d80da3d8acb04939
Polina Tytarenko <polinatitarenko20@gmail.com>
2023-09-19 23:30:31 +0300

    Format files with black

--- social_network/asgi.py
+++ social_network/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_network.settings")
 
 application = get_asgi_application()
