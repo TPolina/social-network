fb0564455a14414e3bb30c2ae04c31b13a1848c5
Polina Tytarenko <polinatitarenko20@gmail.com>
2023-09-21 15:12:00 +0300

    Implement analytics on the number of likes

--- posts/urls.py
+++ posts/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from posts.views import PostViewSet
+from posts.views import PostViewSet, AnalyticsView
 
 router = routers.DefaultRouter()
 router.register("posts", PostViewSet)
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("analytics/", AnalyticsView.as_view(), name="analytics")
 ]
 
 app_name = "posts"
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,8 @@
-from rest_framework import viewsets, status
+from datetime import datetime
+
+from django.db.models import Count
+from django.db.models.functions import TruncDay
+from rest_framework import viewsets, status, generics
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -69,3 +73,47 @@ class PostViewSet(viewsets.ModelViewSet):
             {"detail": "Post wasn't liked"},
             status=status.HTTP_409_CONFLICT
         )
+
+
+class AnalyticsView(generics.GenericAPIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        try:
+            date_from = datetime.strptime(
+                self.request.query_params.get("date_from"), "%Y-%m-%d"
+            )
+            date_to = datetime.strptime(
+                self.request.query_params.get("date_to"), "%Y-%m-%d"
+            )
+        except (TypeError, ValueError):
+            return
+
+        return Like.objects.filter(
+            created_at__range=(date_from, date_to)
+        ).annotate(
+            day=TruncDay("created_at")
+        ).values("day").annotate(likes=Count("id"))
+
+    def get(self, request):
+        queryset = self.get_queryset()
+
+        if queryset is None:
+            return Response(
+                {"detail": "Invalid date format"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        analytics_data = [
+            {"day": entry["day"].strftime("%Y-%m-%d"), "likes": entry["likes"]}
+            for entry in queryset
+        ]
+
+        if not analytics_data:
+            return Response(
+                {"detail": "No likes during this period"},
+                status=status.HTTP_200_OK
+            )
+
+        return Response(analytics_data, status=status.HTTP_200_OK)
