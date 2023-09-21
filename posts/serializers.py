31196ce7c7da3db78d4e355b83bcffe21162d64c
Polina Tytarenko <polinatitarenko20@gmail.com>
2023-09-21 12:23:39 +0300

    Implement post liking/unliking

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from posts.models import Post
+from posts.models import Post, Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -10,3 +10,12 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ("id", "user", "title", "text", "created_at", "likes")
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source="user.username", read_only=True)
+    post = serializers.CharField(source="post.title", read_only=True)
+
+    class Meta:
+        model = Like
+        fields = ("id", "user", "post", "created_at")
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from posts.models import Post
+from posts.models import Post, Like
 from posts.permissions import IsPostAuthorOrReadOnly
-from posts.serializers import PostSerializer
+from posts.serializers import PostSerializer, LikeSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -12,5 +15,57 @@ class PostViewSet(viewsets.ModelViewSet):
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsPostAuthorOrReadOnly,)
 
+    def get_serializer_class(self):
+        if self.action in ("like_post", "unlike_post"):
+            return LikeSerializer
+
+        return PostSerializer
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="like",
+        permission_classes=[IsAuthenticated],
+    )
+    def like_post(self, request, pk=None):
+        post = self.get_object()
+        user = request.user
+
+        like = Like.objects.filter(user=user, post=post).first()
+
+        if not like:
+            new_like = Like.objects.create(post=post, user=user)
+            serializer = self.get_serializer(new_like, many=False)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(
+            {"detail": "Post is already liked"},
+            status=status.HTTP_409_CONFLICT
+        )
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="unlike",
+        permission_classes=[IsAuthenticated],
+    )
+    def unlike_post(self, request, pk=None):
+        post = self.get_object()
+        user = request.user
+
+        like = Like.objects.filter(user=user, post=post).first()
+
+        if like:
+            like.delete()
+            return Response(
+                {"detail": "Post is unliked"},
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            {"detail": "Post wasn't liked"},
+            status=status.HTTP_409_CONFLICT
+        )
